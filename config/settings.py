ecd2f669f89d4f3627692c40df9d3e4650798f81
Akinyemi Marvellous <makinyemi501@gmail.com>
2026-07-25 15:47:03 +0100

    Ready for Railway deployment

--- config/settings.py
+++ config/settings.py
@@ -69,12 +69,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# ── DATABASE ──────────────────────────────────────────────────────────────────
+# ── DATABASE ──
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # ── PASSWORD VALIDATION ──────────────────────────────────────────────────────
